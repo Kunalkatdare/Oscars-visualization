1899d5c151d13673ead897906b91ae16119d6d97
vats512 <vatsal512@gmail.com>
2018-12-18 23:36:07 -0500

    Radar and general fixes

--- js/radarScript.js
+++ js/radarScript.js
@@ -14,7 +14,6 @@ function loadSlider(){
             .on('onchange', val => {
                 year = d3.timeFormat('%Y')(val);
                 updateCharts(year);
-                updateChartsGen(year);
             });
 
         var group3 = d3.select("#containerYearOscarGrossBudget")
@@ -25,109 +24,122 @@ function loadSlider(){
         group3.call(slider3);
 	}
 
-	data_temp = {}
-  data_temp_gross = {}
-	data_sub = {}
-  data_sub_gross = {}
-	function RadarData(){
-			return Promise.all([
-            	d3.csv("data/Oscars-Year-Budget-Gross-Score.csv")
-	        ]).then(datasets => {
-	            data_temp.radarData = datasets[0];
-	            return data_temp;
-	        })
-	}
+loadSlider();
 
-	function convert_radar_Data(radarData){
-		 let result = radarData.reduce((result, d) => {
-	     result[d.title_year] = {
-	            "Budget": (d.budget/212500000)*100,
-	            "Gross Box Office": (d.gross/700000000)*100,
-	            "IMDB Rating": d.imdb_score*10,
-              "Num Critics": (d.num_critics/700)*100,
-              "Duration": (d.duration/240)*100,
-              "Movie Title": (d.movie_title)
-	           }
-	     return result;
-        }, {})
-     console.log("Radar result: ",result);
-		return result;
-	}
+var margin = { top: 50, right: 80, bottom: 50, left: 80 },
+        width =  700,
+        // width = Math.min(700, window.innerWidth / 4) - margin.left - margin.right,
+        height = Math.min(width, window.innerHeight - margin.top - margin.bottom);
 
-	function draw_radar_Data(){
-		let raw_data =  data_temp.radarData
-		data_sub = convert_radar_Data(raw_data)
-		year = "1994"
-		axisData = formatAxisData(data_sub, year)
-    mT = test(data_sub, year);
-    var x = document.getElementById("oscar"); 
-    x.innerHTML = mT;
-    // console.log("DataSub: ", data_sub);
-    RadarChart.draw("#radar", axisData)
-	}
+      let data_temp = {};
+      
+      function test(data, year){
+          var meh = data[year]['Movie Title'];
+          return meh;
+        }
 
-	function formatAxisData(data, year){
-        var d=[
-            [
-                {axis:"IMDB Rating",value:data[year]['IMDB Rating']},
-                {axis:"Num Critics",value:data[year]['Num Critics']},
-                {axis:"Budget",value:data[year]['Budget']},
-                {axis:"Gross Box Office",value:data[year]['Gross Box Office']},
-                {axis:"Duration",value:data[year]['Duration']},
-                // {data:"Movie Title", value:data[year]['Movie Title']}
-            ]
-        ]
-        // console.log("d: ",d)
-        return d
-    }
+      function RadarData(){
+        return Promise.all([
+                d3.csv("data/Oscars-Year-Budget-Gross-Score.csv"),
+                d3.csv("data/highestGross.csv"),
+            ]).then(datasets => {
+                data_temp.radarData = datasets[0];
+                data_temp.radarGrossData = datasets[1];
+                console.log(data_temp);
+                return data_temp;
+            })
+      }
+      
+      function convert_radar_Data(radarData){
 
-    function test(data, year){
-      var meh = data[year]['Movie Title'];
-      // console.log("meh: ", meh);
-      return meh;
-    }
+        console.log(radarData)
+         let result = radarData.reduce((result, d) => {
+           result[d.title_year] = {
+                  "Budget": (d.budget/212500000)*100,
+                  "Gross Box Office": (d.gross/700000000)*100,
+                  "IMDB Rating": d.imdb_score*10,
+                    "Num Critics": (d.num_critics/700)*100,
+                    "Duration": (d.duration/240)*100,
+                    "Movie Title": (d.movie_title)
+                 }
+           return result;
+            }, {})
+         console.log("Radar result: ",result);
+        return result;
+      }
 
-	  function updateCharts(year){
-        axisData = formatAxisData(data_sub, year)
-        mT = test(data_sub, year);
-        var x = document.getElementById("oscar"); 
-        x.innerHTML = mT;
-        RadarChart.draw("#radar", axisData);
-        // console.log("AxisData: ",axisData);
-    }
-    
+      var axisData = [];
+      function formatAxisData(data, data_gross, year){
+             
+            var data_obj = [
+        { name: 'Best Grosser Picture',
+          axes: [
+            {axis: 'IMDB Rating', value: data_gross[year]['IMDB Rating']},
+            {axis: 'Num Critics', value: data_gross[year]['Num Critics']},
+            {axis: 'Budget', value: data_gross[year]['Budget']},
+            {axis: 'Gross Box Office Profit', value: data_gross[year]['Gross Box Office']},
+            {axis: 'Duration', value: data_gross[year]['Duration']}
+          ]
+        },
+        { name: 'Best Picture Award Winner',
+          axes: [
+            {axis: 'IMDB Rating', value: data[year]['IMDB Rating']},
+            {axis: 'Num Critics', value: data[year]['Num Critics']},
+            {axis: 'Budget', value: data[year]['Budget']},
+            {axis: 'Gross Box Office Profit', value: data[year]['Gross Box Office']},
+            {axis: 'Duration', value: data[year]['Duration']}
+          ]
+        }
+        ];
 
-    loadSlider()
-    RadarData().then(draw_radar_Data);
+        return data_obj;
+        }
 
-//GENERAL GRAPH ******************************************************************************88
+        let radar_data = {}
+        let radar_gross_data = {}
+      function draw_radar_Data(){
+        radar_data =  data_temp.radarData;
+        radar_gross_data = data_temp.radarGrossData;
+        console.log(radar_data);
+        radar_data_conv = convert_radar_Data(radar_data)
+        radar_gross_data_conv = convert_radar_Data(radar_gross_data)
+        year = "1994"
+        axisData = formatAxisData(radar_data_conv, radar_gross_data_conv, year)
+          mT = test(radar_data_conv, year);
+          var x = document.getElementById("oscar"); 
+          x.innerHTML = mT;
 
-    function RadarDataGen(){
-      return Promise.all([
-                d3.csv("data/highestGross.csv"), //change this CSV file
-          ]).then(datasets => {
-              data_temp_gross.radarData = datasets[0];
-              return data_temp_gross;
-          })
-    }
+          mT = test(radar_gross_data_conv, year);
+          var x = document.getElementById("gross"); 
+          x.innerHTML = mT;
 
-    function draw_radar_Data_Gen(){
-      let raw_data =  data_temp_gross.radarData
-      data_sub_gross = convert_radar_Data(raw_data)
-      year = "1994"
-      axisData = formatAxisData(data_sub_gross, year)
-      RadarChart.draw("#radarGen", axisData)
-      mT = test(data_sub_gross, year);
-      var x = document.getElementById("gross"); 
-      x.innerHTML = mT;
-    }
-    function updateChartsGen(year){
-        axisData = formatAxisData(data_sub_gross, year)
-        RadarChart.draw("#radarGen", axisData)
-        mT = test(data_sub_gross, year);
-        var x = document.getElementById("gross"); 
-        x.innerHTML = mT;
-        // d3.select(#oscar).append("h3");
-    }
+          let svg_radar1 = RadarChart(".radarChart", axisData, radarChartOptions);
+      }
 
-    RadarDataGen().then(draw_radar_Data_Gen);
+
+
+      var radarChartOptions = {
+        w: 290,
+        h: 350,
+        margin: margin,
+        levels: 5,
+        roundStrokes: true,
+        color: d3.scaleOrdinal().range(["#26AF32", "#762712"]),
+        format: '.0f',
+        legend: { title: 'Organization XYZ', translateX: 100, translateY: 40 }
+      };
+
+      function updateCharts(year){
+            axisData = formatAxisData(radar_data_conv, radar_gross_data_conv, year)
+          mT = test(radar_data_conv, year);
+          var x = document.getElementById("oscar"); 
+          x.innerHTML = mT;
+
+          mT = test(radar_gross_data_conv, year);
+          var x = document.getElementById("gross"); 
+          x.innerHTML = mT;
+
+          let svg_radar1 = RadarChart(".radarChart", axisData, radarChartOptions);
+        }
+
+      RadarData().then(draw_radar_Data);
